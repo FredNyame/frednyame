0982e683c5806edc179a1d849f7a071890dbf52a
fredNyame <frednyame63@gmail.com>
2019-04-20 19:01:09 -0400

    Updates

---
+++ precache-manifest.9121de4ba8b7bbb9c3d0999a77411901.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "8571ad70f631a140acf74ea2ee395bdb",
+    "url": "/mywebsite/static/media/church.8571ad70.png"
+  },
+  {
+    "revision": "b667a07dffa52cc4b39d",
+    "url": "/mywebsite/static/css/main.8a09ff0b.chunk.css"
+  },
+  {
+    "revision": "1029a0e86ffa031e1963",
+    "url": "/mywebsite/static/js/runtime~main.8e5ad122.js"
+  },
+  {
+    "revision": "3cf68b544d74c139dd94",
+    "url": "/mywebsite/static/js/2.f9e9f58f.chunk.js"
+  },
+  {
+    "revision": "b1c2410fe7e9017e92ded322d0a3517a",
+    "url": "/mywebsite/static/media/poster.b1c2410f.png"
+  },
+  {
+    "revision": "e75392d0cd4f37dfd76aa690b21a1a90",
+    "url": "/mywebsite/static/media/menu.e75392d0.png"
+  },
+  {
+    "revision": "e12052c1db4ac8d594eb451abf696a5c",
+    "url": "/mywebsite/static/media/adinkra.e12052c1.png"
+  },
+  {
+    "revision": "44b8bb5c5a858670118f63f0af3992ab",
+    "url": "/mywebsite/static/media/github.44b8bb5c.svg"
+  },
+  {
+    "revision": "78c03c1e764ca369651ddb8443c6bcc2",
+    "url": "/mywebsite/static/media/instagram.78c03c1e.svg"
+  },
+  {
+    "revision": "87bfc8f5fe2760bb315283857cf68e92",
+    "url": "/mywebsite/static/media/linkedin.87bfc8f5.svg"
+  },
+  {
+    "revision": "0e2c42a36ec0645243788227c2e1c246",
+    "url": "/mywebsite/static/media/behance.0e2c42a3.svg"
+  },
+  {
+    "revision": "f262e8f8905b3222435f515284bb2999",
+    "url": "/mywebsite/static/media/dribbble.f262e8f8.svg"
+  },
+  {
+    "revision": "a0086c79d4a8b53be35704a9154bee83",
+    "url": "/mywebsite/static/media/arrow-down.a0086c79.svg"
+  },
+  {
+    "revision": "d305b977a1e4a0f6e242836528078a35",
+    "url": "/mywebsite/static/media/Gsenmobile.d305b977.png"
+  },
+  {
+    "revision": "65b87488e44a5f29cef41688676129c1",
+    "url": "/mywebsite/static/media/alexhomemockup.65b87488.png"
+  },
+  {
+    "revision": "a20ea5eadcf4d6e1918aed7706898bdb",
+    "url": "/mywebsite/static/media/screenhome.a20ea5ea.png"
+  },
+  {
+    "revision": "7f11593a4c0bf2a2cdfcbf7b94f0f76c",
+    "url": "/mywebsite/static/media/profilepic.7f11593a.png"
+  },
+  {
+    "revision": "9ab1b1841eb661af6373825bdd8fa51a",
+    "url": "/mywebsite/static/media/shipping.9ab1b184.png"
+  },
+  {
+    "revision": "360e4273544a0ce703b85fea43ccc75f",
+    "url": "/mywebsite/static/media/Gsen-mockup.360e4273.png"
+  },
+  {
+    "revision": "b667a07dffa52cc4b39d",
+    "url": "/mywebsite/static/js/main.6bfd1944.chunk.js"
+  },
+  {
+    "revision": "74a0a2289c0c1456a40621fc8e9786d0",
+    "url": "/mywebsite/static/media/xmas.74a0a228.png"
+  },
+  {
+    "revision": "0dd14553b64dd814ece29a12dfa369e5",
+    "url": "/mywebsite/static/media/Xmasv3.0dd14553.png"
+  },
+  {
+    "revision": "98e5f392fc3aae921c50477376c87358",
+    "url": "/mywebsite/static/media/ladcom.98e5f392.png"
+  },
+  {
+    "revision": "d025d220a958e5665f062056bae0a56d",
+    "url": "/mywebsite/static/media/gsenabovefold.d025d220.jpg"
+  },
+  {
+    "revision": "42b9e24acf322a84cd9e2d4205520251",
+    "url": "/mywebsite/static/media/gsenprojectlogo.42b9e24a.png"
+  },
+  {
+    "revision": "0c5cd0f9e2b4cdba993effebaf41f3de",
+    "url": "/mywebsite/static/media/gsenmainhome.0c5cd0f9.png"
+  },
+  {
+    "revision": "65c672dd1438aef4c92d3cfee12b376a",
+    "url": "/mywebsite/static/media/gsenprojectmobile.65c672dd.png"
+  },
+  {
+    "revision": "90f972e7346cb8ccc0d2ad4104b0b7f3",
+    "url": "/mywebsite/static/media/Shipmain.90f972e7.jpg"
+  },
+  {
+    "revision": "704866039cbd13ba0b12345de12fe1ff",
+    "url": "/mywebsite/static/media/Shiplogo.70486603.png"
+  },
+  {
+    "revision": "f89bbcca3f69b0bc187ece9efd476dd0",
+    "url": "/mywebsite/static/media/ShipFull.f89bbcca.jpg"
+  },
+  {
+    "revision": "6fc418f348e39bf6ddda04161cadef01",
+    "url": "/mywebsite/static/media/Shipmobile.6fc418f3.jpg"
+  },
+  {
+    "revision": "b38a3bbf8c8bca572d3ab0d9aafcc08e",
+    "url": "/mywebsite/static/media/Churchmain.b38a3bbf.jpg"
+  },
+  {
+    "revision": "634864e47c66b06a9804ccea7a64f6e3",
+    "url": "/mywebsite/static/media/ChurchFull.634864e4.jpg"
+  },
+  {
+    "revision": "f75c76be661f48a496229657816dc88d",
+    "url": "/mywebsite/static/media/ladcomhome.f75c76be.png"
+  },
+  {
+    "revision": "d9895fbabdf7d148ed916fbb2dcd9158",
+    "url": "/mywebsite/static/media/ladcomfullpage.d9895fba.png"
+  },
+  {
+    "revision": "90dd7bdd7c15769b1a7c7b95229c2e3f",
+    "url": "/mywebsite/static/media/SignPainter-HouseScript.90dd7bdd.ttf"
+  },
+  {
+    "revision": "ee64fb9d3f1ba2333e1b489283925bce",
+    "url": "/mywebsite/static/media/futura medium bt.ee64fb9d.ttf"
+  },
+  {
+    "revision": "b1abb878e2529cb5cb4450139844155d",
+    "url": "/mywebsite/static/media/AvenirLTStd-Black.b1abb878.otf"
+  },
+  {
+    "revision": "03f73d6d98b242f8e57d5c7fc1a69a67",
+    "url": "/mywebsite/index.html"
+  }
+];
