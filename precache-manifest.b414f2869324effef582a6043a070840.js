246c4a8cae78f8fc1efefe6548cdcce76aac2f58
fredNyame <frednyame63@gmail.com>
2019-04-20 22:08:25 -0400

    Updates

---
+++ precache-manifest.b414f2869324effef582a6043a070840.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "8571ad70f631a140acf74ea2ee395bdb",
+    "url": "/portfolio/static/media/church.8571ad70.png"
+  },
+  {
+    "revision": "5a20e14015ef7cd02241",
+    "url": "/portfolio/static/css/main.d4a87a2b.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "3cf68b544d74c139dd94",
+    "url": "/portfolio/static/js/2.f9e9f58f.chunk.js"
+  },
+  {
+    "revision": "b1c2410fe7e9017e92ded322d0a3517a",
+    "url": "/portfolio/static/media/poster.b1c2410f.png"
+  },
+  {
+    "revision": "e75392d0cd4f37dfd76aa690b21a1a90",
+    "url": "/portfolio/static/media/menu.e75392d0.png"
+  },
+  {
+    "revision": "e12052c1db4ac8d594eb451abf696a5c",
+    "url": "/portfolio/static/media/adinkra.e12052c1.png"
+  },
+  {
+    "revision": "44b8bb5c5a858670118f63f0af3992ab",
+    "url": "/portfolio/static/media/github.44b8bb5c.svg"
+  },
+  {
+    "revision": "78c03c1e764ca369651ddb8443c6bcc2",
+    "url": "/portfolio/static/media/instagram.78c03c1e.svg"
+  },
+  {
+    "revision": "87bfc8f5fe2760bb315283857cf68e92",
+    "url": "/portfolio/static/media/linkedin.87bfc8f5.svg"
+  },
+  {
+    "revision": "0e2c42a36ec0645243788227c2e1c246",
+    "url": "/portfolio/static/media/behance.0e2c42a3.svg"
+  },
+  {
+    "revision": "f262e8f8905b3222435f515284bb2999",
+    "url": "/portfolio/static/media/dribbble.f262e8f8.svg"
+  },
+  {
+    "revision": "a0086c79d4a8b53be35704a9154bee83",
+    "url": "/portfolio/static/media/arrow-down.a0086c79.svg"
+  },
+  {
+    "revision": "d305b977a1e4a0f6e242836528078a35",
+    "url": "/portfolio/static/media/Gsenmobile.d305b977.png"
+  },
+  {
+    "revision": "65b87488e44a5f29cef41688676129c1",
+    "url": "/portfolio/static/media/alexhomemockup.65b87488.png"
+  },
+  {
+    "revision": "a20ea5eadcf4d6e1918aed7706898bdb",
+    "url": "/portfolio/static/media/screenhome.a20ea5ea.png"
+  },
+  {
+    "revision": "7f11593a4c0bf2a2cdfcbf7b94f0f76c",
+    "url": "/portfolio/static/media/profilepic.7f11593a.png"
+  },
+  {
+    "revision": "9ab1b1841eb661af6373825bdd8fa51a",
+    "url": "/portfolio/static/media/shipping.9ab1b184.png"
+  },
+  {
+    "revision": "360e4273544a0ce703b85fea43ccc75f",
+    "url": "/portfolio/static/media/Gsen-mockup.360e4273.png"
+  },
+  {
+    "revision": "5a20e14015ef7cd02241",
+    "url": "/portfolio/static/js/main.be1c338d.chunk.js"
+  },
+  {
+    "revision": "74a0a2289c0c1456a40621fc8e9786d0",
+    "url": "/portfolio/static/media/xmas.74a0a228.png"
+  },
+  {
+    "revision": "0dd14553b64dd814ece29a12dfa369e5",
+    "url": "/portfolio/static/media/Xmasv3.0dd14553.png"
+  },
+  {
+    "revision": "98e5f392fc3aae921c50477376c87358",
+    "url": "/portfolio/static/media/ladcom.98e5f392.png"
+  },
+  {
+    "revision": "d025d220a958e5665f062056bae0a56d",
+    "url": "/portfolio/static/media/gsenabovefold.d025d220.jpg"
+  },
+  {
+    "revision": "42b9e24acf322a84cd9e2d4205520251",
+    "url": "/portfolio/static/media/gsenprojectlogo.42b9e24a.png"
+  },
+  {
+    "revision": "0c5cd0f9e2b4cdba993effebaf41f3de",
+    "url": "/portfolio/static/media/gsenmainhome.0c5cd0f9.png"
+  },
+  {
+    "revision": "65c672dd1438aef4c92d3cfee12b376a",
+    "url": "/portfolio/static/media/gsenprojectmobile.65c672dd.png"
+  },
+  {
+    "revision": "90f972e7346cb8ccc0d2ad4104b0b7f3",
+    "url": "/portfolio/static/media/Shipmain.90f972e7.jpg"
+  },
+  {
+    "revision": "704866039cbd13ba0b12345de12fe1ff",
+    "url": "/portfolio/static/media/Shiplogo.70486603.png"
+  },
+  {
+    "revision": "f89bbcca3f69b0bc187ece9efd476dd0",
+    "url": "/portfolio/static/media/ShipFull.f89bbcca.jpg"
+  },
+  {
+    "revision": "6fc418f348e39bf6ddda04161cadef01",
+    "url": "/portfolio/static/media/Shipmobile.6fc418f3.jpg"
+  },
+  {
+    "revision": "b38a3bbf8c8bca572d3ab0d9aafcc08e",
+    "url": "/portfolio/static/media/Churchmain.b38a3bbf.jpg"
+  },
+  {
+    "revision": "634864e47c66b06a9804ccea7a64f6e3",
+    "url": "/portfolio/static/media/ChurchFull.634864e4.jpg"
+  },
+  {
+    "revision": "f75c76be661f48a496229657816dc88d",
+    "url": "/portfolio/static/media/ladcomhome.f75c76be.png"
+  },
+  {
+    "revision": "d9895fbabdf7d148ed916fbb2dcd9158",
+    "url": "/portfolio/static/media/ladcomfullpage.d9895fba.png"
+  },
+  {
+    "revision": "90dd7bdd7c15769b1a7c7b95229c2e3f",
+    "url": "/portfolio/static/media/SignPainter-HouseScript.90dd7bdd.ttf"
+  },
+  {
+    "revision": "ee64fb9d3f1ba2333e1b489283925bce",
+    "url": "/portfolio/static/media/futura medium bt.ee64fb9d.ttf"
+  },
+  {
+    "revision": "b1abb878e2529cb5cb4450139844155d",
+    "url": "/portfolio/static/media/AvenirLTStd-Black.b1abb878.otf"
+  },
+  {
+    "revision": "55e627733a980e5f3ae621b6d757815f",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b9115a0bf5057cfc486785f557099a4.js"
+  "/portfolio/precache-manifest.b414f2869324effef582a6043a070840.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
